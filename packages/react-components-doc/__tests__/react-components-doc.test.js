ec9bb7cdf449074c1c5dd1520fa4d61f19b4dec1
Bigme2020 <2606249370@qq.com>
2023-01-27 22:45:20 +0800

    feat: add react-component doc

--- packages/react-components-doc/__tests__/react-components-doc.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const reactComponentsDoc = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(reactComponentsDoc(), 'Hello from reactComponentsDoc');
-console.info("reactComponentsDoc tests passed");
